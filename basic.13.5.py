19573ab8a911cbfbd40ab642064120e419fbbe49
Patrick Callahan <114837562+pcallahandoescs@users.noreply.github.com>
2023-03-29 00:17:42 -0500

    Add files via upload

---
+++ basic.13.5.py
@@ -0,0 +1,11 @@
+#GREATER THAN Y
+#given an array and a value Y, count and print the number ...
+#of array values greater than Y
+
+import array
+array_Y = array.array('i', [1,4,67,7,45,666,9])
+
+Y = 30
+for i in array_Y:
+    if i > Y:
+        print(i)
